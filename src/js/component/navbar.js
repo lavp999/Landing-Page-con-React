06a603bd5c8ec9b7b29f53718681965ccb918d45
Luis <lavp99@hotmail.com>
2022-11-03 00:39:20 +0100

    avanzado muy poco

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,23 +5,22 @@ const NavBar = () => {
 	return (
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Navbar w/ text</a>
+                <a className="navbar-brand" href="#">Start Bootstrap</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarText">
+                <div className="collapse navbar-collapse menuDerecha" id="navbarText">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item active">
                             <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
+                            <a className="nav-link" href="#">About</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
+                            <a className="nav-link" href="#">Contact</a>
                         </li>
                     </ul>
-                    <span className="navbar-text">Navbar text with an inline element</span>
                 </div>
             </nav>
         </>
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,9 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.js";
+import NavBar from "./component/navbar";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
+
+ReactDOM.render(<NavBar />, document.querySelector("#app2"));
