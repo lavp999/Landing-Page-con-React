fe4b174e17d0b3f45222a6249f63050ebaf158cf
Luis <lavp99@hotmail.com>
2022-11-05 00:30:09 +0100

    subida

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -2,14 +2,16 @@ import React from "react";
 
 const Card = () => {
 	return (
-        <div className="card">
-            <img src="../" className="card-img-top" alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">texto de la tarjeta</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+        <>
+            <div className="card">
+                <img src="http://via.placeholder.com/350x150" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">texto de la tarjeta</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
             </div>
-        </div>
+        </>
 	);
 };
 
--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const Jumbotron = () => {
 	return (
+        <>
             <div className="jumbotron">
                 <h1 className="display-4">Hello, world!</h1>
                 <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
@@ -11,6 +12,7 @@ const Jumbotron = () => {
                     <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
                 </p>
             </div>
+        </>
 	);
 };
 
