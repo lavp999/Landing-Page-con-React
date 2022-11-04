e17f4e9d49aa77a761f4e2ce483726d85e09350d
Luis <lavp99@hotmail.com>
2022-11-05 00:30:00 +0100

    un opaso mas

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,25 +3,35 @@ import React from "react";
 //create your first component
 const NavBar = () => {
 	return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">Start Bootstrap</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse menuDerecha" id="navbarText">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Contact</a>
-                        </li>
-                    </ul>
+        <>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="row ">
+                    <div className="col-1">
+                        <a className="navbar-brand" href="#">Bootsatrap</a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+                    <div className="col-7">   <span></span>        </div>
+                    <div className="col-4 collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">about</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Services</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
+        </>
 	);
 };
 
