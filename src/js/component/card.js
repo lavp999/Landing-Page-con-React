53466e48f315c39102e7e41df370e55716c2969f
Luis <lavp99@hotmail.com>
2022-11-04 18:44:27 +0100

    connuna clase sobrescrita CARD

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -3,10 +3,10 @@ import React from "react";
 const Card = () => {
 	return (
         <div className="card">
-            <img className="card-img-top" src="../../img/gris.svg" alt="Card image cap" />
+            <img src="../" className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card-text">texto de la tarjeta</p>
                 <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
