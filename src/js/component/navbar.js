fe68e69d57041f1e1ed0ec28f6092c8aa4e7e680
lavp999 <lavp99@hotmail.com>
2022-11-16 19:32:06 +0000

    para entregar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -11,9 +11,7 @@ const NavBar = () => {
   return (
     <>
       <nav className="container navbar navbar-expand-lg navbar-dark bg-dark d-flex position-relative">
-        <a className="navbar-brand" href="#">
-          Start Bootstrap
-        </a>
+        <a className="navbar-brand" href="#">Start Bootstrap</a>
         <button
           className="navbar-toggler position-absolute end-0"
           type="button"
@@ -26,48 +24,33 @@ const NavBar = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div className="justify-content-end w-100 borde d-md-none d-sm-none d-lg-flex d-xl-flex d-xxl-flex">
+        <div className="justify-content-end w-100 d-md-none d-sm-none d-lg-flex d-xl-flex d-xxl-flex">
           <ul className="nav justify-content-end">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                about
-              </a>
+              <a className="nav-link" href="#">about</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Services
-              </a>
+              <a className="nav-link" href="#">Services</a>
             </li>
             <li className="nav-item">
               <a className="nav-link disabled">Contact</a>
             </li>
           </ul>
         </div>
-        <div className="d-md-flex d-sm-flex d-lg-none d-xl-none d-xxl-none justify-content-end">
-          <div
-            className="p-2 collapse navbar-collapse"
-            id="navbarSupportedContent"
-          >
-            <ul className="nav justify-content-end">
+        <div className="d-md-flex d-sm-flex d-lg-none d-xl-none d-xxl-none">
+          <div className="p-2 collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="nav navbar-nav mr-auto mt-2 mt-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
-                  Home
-                </a>
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  about
-                </a>
+                <a className="nav-link" href="#">about</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Services
-                </a>
+                <a className="nav-link" href="#">Services</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link disabled">Contact</a>
